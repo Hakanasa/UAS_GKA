cb020bd1a6ca1d98ab6951a6cd926a730e1ffb3e
Kleight <martinwongso234@gmail.com>
2020-11-20 21:08:03 +0700

    Tes

---
+++ UAS-GKA/Assets/Scripts/Camera_Changer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Changer : MonoBehaviour
+{
+    [SerializeField] GameObject[] Camera;
+
+    private void Start()
+    {
+        StartCoroutine(ChangeCamera());
+    }
+
+    IEnumerator ChangeCamera()
+    {
+        int i = 0;
+        for(; ;)
+        {
+            if (i == Camera.Length)
+            {
+                i = 0;
+            }
+            if(i < 1)
+                Camera[i].SetActive(true);
+            else
+            {
+                Camera[i-1].SetActive(false);
+                Camera[i].SetActive(true);
+            }
+            i++;
+            yield return new WaitForSeconds(4f);
+        }
+    }
+}
