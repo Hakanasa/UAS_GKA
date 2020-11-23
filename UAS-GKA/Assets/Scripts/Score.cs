754807936f2596463f770f610be626b122790686
Hakanasa <louisjonathan29@gmail.com>
2020-11-23 15:09:39 +0700

    Counter Score + ANimasi title

---
+++ UAS-GKA/Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    GameObject count;
+    int jumlah;
+    // Start is called before the first frame update
+    void Start()
+    {
+       jumlah = (GameObject.FindGameObjectsWithTag("Cheese").Length);
+        count = GameObject.Find("Canvas/Left");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jumlah = (GameObject.FindGameObjectsWithTag("Cheese").Length);
+        count.GetComponent<Text>().text = jumlah.ToString();
+    }
+}
